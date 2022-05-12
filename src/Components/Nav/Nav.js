53d3962020ce12da4de703664e6acd1228ffbb8b
Loïs <flojuve@hotmail.fr>
2022-05-12 15:00:20 +0200

    style update

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,22 +3,24 @@ import logo from "../../img/argentBankLogo.png";
 
 function Nav() {
   return (
-    <nav className="main-nav">
-      <Link className="main-nav-logo" to="/">
-        <img
-          className="main-nav-logo-image"
-          alt="Argent Bank Logo"
-          src={logo}
-        />
-        <h1 className="sr-only">Argent Bank</h1>
-      </Link>
-      <div>
-        <Link className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
+    <header>
+      <nav className="main-nav">
+        <Link className="main-nav-logo" to="/">
+          <img
+            className="main-nav-logo-image"
+            alt="Argent Bank Logo"
+            src={logo}
+          />
+          <h1 className="sr-only">Argent Bank</h1>
         </Link>
-      </div>
-    </nav>
+        <div>
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
 export default Nav;
